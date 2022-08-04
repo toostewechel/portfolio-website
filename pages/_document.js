7a75f41158fb0b2c3639aade78668b655c215eaa
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-04 11:27:01 +0000

    _document page setup for global typography (google fonts)

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400&family=Vollkorn:wght@600;800&display=swap" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
