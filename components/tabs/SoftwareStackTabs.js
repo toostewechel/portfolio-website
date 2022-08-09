748b01e2356595bf310e4ee33dd2c856ce146fb9
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-09 17:26:40 +0000

    fill workflow tab with apps and tools

--- components/tabs/SoftwareStackTabs.js
+++ components/tabs/SoftwareStackTabs.js
@@ -93,8 +93,8 @@ export default () => (
           <StyledLink
             href="https://spline.design/"
             src="/logo/spline3d-logo.png"
-            title="Adobe Illustrator"
-            description="Design tool for 3D web browser experiences"
+            title="Spline 3D"
+            description="Design tool for 3D web experiences"
           />
         </Flex>
       </FlexContainer>
@@ -109,7 +109,7 @@ export default () => (
             description="The collaborative browser based IDE"
           />
           <StyledLink
-            href="https://www.adobe.com/products/illustrator.html"
+            href="https://code.visualstudio.com/"
             src="/logo/visual-studio-code-logo.png"
             title="Visual Studio Code"
             description="Code editor (IDE)"
@@ -118,13 +118,13 @@ export default () => (
             href="https://reactjs.org/"
             src="/logo/react-logo.png"
             title="React"
-            description="JavaScript library for building user interfaces"
+            description="JavaScript library for building interfaces"
           />
           <StyledLink
             href="https://www.radix-ui.com/"
             src="/logo/radix-logo.png"
             title="Radix UI"
-            description="Component library for React"
+            description="Unstyled component library for React"
           />
         </Flex>
         <Flex>
@@ -132,7 +132,7 @@ export default () => (
             href="https://www.github.com"
             src="/logo/github-logo.png"
             title="GitHub"
-            description="Software development and version control"
+            description="Software development & version control"
           />
           <StyledLink
             href="https://hyper.is/"
@@ -153,30 +153,66 @@ export default () => (
       <FlexContainer>
         <Flex>
           <StyledLink
-            href="https://www.figma.com/"
-            src="/logo/figma-logo.png"
-            title="Figma"
-            description="The collaborative interface design tool"
+            href="https://linear.app/"
+            src="/logo/linear-logo.png"
+            title="Linear"
+            description="Issue tracking tool"
           />
           <StyledLink
             href="https://www.adobe.com/products/illustrator.html"
-            src="/logo/adobe-illustrator-logo.png"
-            title="Adobe Illustrator"
-            description="Vector based graphical design tool"
+            src="/logo/iconjar-logo.png"
+            title="IconJar"
+            description="Icon organizer for macOS"
+          />
+          <StyledLink
+            href="https://www.notion.so/"
+            src="/logo/notion-logo.png"
+            title="Notion"
+            description="Workspace for notes, docs and tasks"
+          />
+          <StyledLink
+            href="https://codespace.app/"
+            src="/logo/codespace-logo.png"
+            title="Codespace"
+            description="Code snippets manager"
+          />
+          <StyledLink
+            href="https://roamresearch.com/"
+            src="/logo/roam-research-logo.png"
+            title="Roam Research"
+            description="Tool for networked thoughts"
           />
         </Flex>
         <Flex>
           <StyledLink
-            href="https://www.adobe.com/products/photoshop.html"
-            src="/logo/adobe-photoshop-logo.png"
-            title="Adobe Photoshop"
-            description="Pixel based graphical design tool"
+            href="https://1password.com/"
+            src="/logo/1password-logo.png"
+            title="1Password"
+            description="Password manager"
           />
           <StyledLink
-            href="https://spline.design/"
-            src="/logo/spline3d-logo.png"
-            title="Adobe Illustrator"
-            description="Design tool for 3D web browser experiences"
+            href="https://wormhole.app/"
+            src="/logo/wormhole-logo.png"
+            title="Wormhole"
+            description="E2E encrypted file sharing"
+          />
+          <StyledLink
+            href="https://signal.org/"
+            src="/logo/signal-logo.png"
+            title="Signal"
+            description="E2E encrypted messaging"
+          />
+          <StyledLink
+            href="https://skiff.com/"
+            src="/logo/skiff-logo.png"
+            title="Skiff"
+            description="E2E encrypted mail, pages and drive"
+          />
+          <StyledLink
+            href="https://raycast.com/"
+            src="/logo/raycast-logo.png"
+            title="Raycast"
+            description="Supercharge productivity"
           />
         </Flex>
       </FlexContainer>
