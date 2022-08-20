d5b477d72380e988acb94e67704cbdd1418b9a7c
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-20 13:12:56 +0000

    create heading component

---
+++ components/typography/Heading.js
@@ -0,0 +1,47 @@
+import { styled } from "../../stitches.config.js";
+
+const StyledHeading = styled("h1", {
+  fontFamily: '$header',
+  fontWeight: '$extra-bold',
+  lineHeight: '$compact',
+  color: '$mauve12',
+  m: 0,
+
+  variants: {
+    level: {
+      1: {
+        fontSize: "$5xl",
+        fontWeight: '$extra-bold',
+        letterSpacing: '$tracking-tighter',
+      },
+      2: {
+        fontSize: "$4xl",
+        fontWeight: '$extra-bold',
+        letterSpacing: '$tracking-tighter',
+      },
+      3: {
+        fontSize: "$3xl",
+        fontWeight: '$extra-bold',
+        letterSpacing: '$tracking-tight',
+      },
+      4: {
+        fontSize: "$2xl",
+        fontWeight: '$semi-bold',
+        letterSpacing: '$tracking-tight',
+      },
+      5: {
+        fontSize: "$xl",
+        fontWeight: '$semi-bold',
+        letterSpacing: '$tracking-normal',
+      }
+    }
+  }
+})
+
+function Heading(props) {
+  return (
+    <StyledHeading as={props.as} level={props.level}>{props.title}</StyledHeading>
+  )
+}
+
+export default Heading;
