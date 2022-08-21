d38f7a03814d01b418e2833cfd69a311aaf2bf1c
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-21 17:28:36 +0000

    extract global tabs component

---
+++ components/tabs/Tabs.js
@@ -0,0 +1,46 @@
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import { styled } from "../../stitches.config.js";
+import ListItem from "../../components/ListItem.js";
+
+const StyledTabs = styled(TabsPrimitive.Root, {
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+});
+
+const StyledList = styled(TabsPrimitive.List, {
+  display: "flex",
+  borderBottom: "1px solid $gray6",
+  mb: "$spacing-03",
+});
+
+const StyledTrigger = styled(TabsPrimitive.Trigger, {
+  all: "unset",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  fontFamily: "default",
+  fontWeight: "$semi-bold",
+  fontSize: "$base",
+  padding: "$spacing-04",
+  color: "$gray11",
+  "&:hover": { color: "$violet11" },
+  '&[data-state="active"]': {
+    color: "$violet11",
+    boxShadow: "inset 0 -2px 0 0 currentColor, 0 2px 0 0 currentColor",
+  },
+  "@bp1": {
+    padding: "$spacing-05 $spacing-06",
+    fontSize: "$lg",
+  },
+});
+
+const StyledContent = styled(TabsPrimitive.Content, {
+  backgroundColor: "white",
+});
+
+// Exports
+export const Tabs = StyledTabs;
+export const TabsList = StyledList;
+export const TabsTrigger = StyledTrigger;
+export const TabsContent = StyledContent;
