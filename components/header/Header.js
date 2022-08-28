0328ad80710a70b8af41b12840ab475ab225ca2e
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-28 17:33:39 +0000

    make headers consistent

--- components/header/Header.js
+++ components/header/Header.js
@@ -35,20 +35,44 @@ const IconBox = styled("div", {
 });
 
 const Title = styled("p", {
+  display: "flex",
+  alignItems: "center",
+  gap: "$spacing-03",
   fontFamily: "$header",
   fontWeight: "$extra-bold",
   lineHeight: "$compact",
   letterSpacing: "$tracking-tighter",
   color: "$mauve12",
-  fontSize: "$3xl",
+  fontSize: "$2xl",
   mt: "$spacing-02",
+
+  "@bp2": {
+    fontSize: "$3xl",
+  },
+});
+
+const StyledSpan = styled("span", {
+  fontFamily: "$header",
+  fontWeight: "$extra-bold",
+  lineHeight: "$compact",
+  letterSpacing: "$tracking-normal",
+  fontSize: "$lg",
+  backgroundColor: "$crimsonA8",
+  pr: "8px",
+  pl: "8px",
+  pt: "6px",
+  pb: "4px",
+  borderRadius: "4px",
+  color: "$crimson2",
+  boxShadow: "$smooth",
+  mt: "-8px",
 });
 
 function Header() {
   return (
     <Container>
       <Box>
-        <Title>Snapshots /</Title>
+        <Title>Snapshots / <StyledSpan> Home </StyledSpan> </Title>
       </Box>
       <Toolbar />
     </Container>
