3d0da8bfacaa91f72225b64e7802dd3f785d401a
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-30 12:48:24 +0000

    update spacer component with padding levels

--- components/layouts/blocks/Spacer.js
+++ components/layouts/blocks/Spacer.js
@@ -2,10 +2,52 @@ import { styled } from "../../../stitches.config.js";
 
 const StyledSpacer = styled("div", {
   p: "$spacing-07 $none",
+
+  variants : {
+    level : {
+      1: {
+        p: "$spacing-01 $none",
+      },
+      2: {
+        p: "$spacing-02 $none",
+      },
+      3: {
+        p: "$spacing-03 $none",
+      },
+      4: {
+        p: "$spacing-04 $none",
+      },
+      5: {
+        p: "$spacing-05 $none",
+      },
+      6: {
+        p: "$spacing-06 $none",
+      },
+      7: {
+        p: "$spacing-07 $none",
+      },
+      8: {
+        p: "$spacing-08 $none",
+      },
+      9: {
+        p: "$spacing-09 $none",
+      },
+      10: {
+        p: "$spacing-10 $none",
+      },
+      11: {
+        p: "$spacing-11 $none",
+      },
+      12: {
+        p: "$spacing-12 $none",
+      },
+      
+    }
+  }
 });
 
-function Spacer() {
-  return <StyledSpacer />;
+function Spacer(props) {
+  return <StyledSpacer level={props.level}>{props.children}</StyledSpacer>;
 }
 
 export default Spacer;
