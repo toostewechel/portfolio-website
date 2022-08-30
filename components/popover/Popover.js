a82b468adb3aadc41f700658dc38447b124c030b
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-08-30 09:05:42 +0000

    style Popover component

---
+++ components/popover/Popover.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { styled, keyframes } from "../../stitches.config.js";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+
+const slideUpAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const slideRightAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+});
+
+const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' },
+});
+
+const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' },
+});
+
+const StyledContent = styled(PopoverPrimitive.Content, {
+  borderRadius: "4px",
+  padding: "$spacing-04",
+  width: "196px",
+  backgroundColor: "white",
+  boxShadow: 'hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px',
+  '@media (prefers-reduced-motion: no-preference)': {
+    animationDuration: '400ms',
+    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+    willChange: 'transform, opacity',
+    '&[data-state="open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade },
+    },
+  },
+})
+
+const StyledArrow = styled(PopoverPrimitive.Arrow, {
+  fill: "white",
+})
+
+function Content({ children, ...props }) {
+  return (
+    <PopoverPrimitive.Portal>
+      <StyledContent {...props}>
+        {children}
+        <StyledArrow />
+      </StyledContent>
+    </PopoverPrimitive.Portal>
+  )
+}
+
+const StyledClose = styled(PopoverPrimitive.Close, {
+  all: "unset",
+  borderRadius: "4px",
+  height: "32px",
+  width: "32px",
+  display: "inline-flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "$violet11",
+  position: "absolute",
+  top: 12,
+  right: 12,
+
+  '&:hover': { backgroundColor: "$violet4" },
+  '&:focus': { boxShadow: "0 0 0 2px $violet7" },
+})
+
+export const Popover = PopoverPrimitive.Root;
+export const PopoverTrigger = PopoverPrimitive.Trigger;
+export const PopoverContent = Content;
+export const PopoverClose = StyledClose;
