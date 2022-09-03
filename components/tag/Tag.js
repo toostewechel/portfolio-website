e0d173becf4d59627798741040136633f0170667
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-09-03 11:39:31 +0000

    fix Tag styling

--- components/tag/Tag.js
+++ components/tag/Tag.js
@@ -3,9 +3,10 @@ import { styled } from "../../stitches.config.js";
 const TagContainer = styled("div", {
   display: "inline-flex",
   borderRadius: "16px",
-  padding: "$spacing-03 $spacing-05",
+  padding: "$spacing-02 $spacing-05",
   backgroundColor: "$gray3",
   color: "$gray11",
+  flexShrink: 0,
 
   variants: {
     color: {
