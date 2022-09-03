01ec46dc26027e5a01a4e3ca1695e82b84b7bd74
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-09-03 08:44:32 +0000

    update header styling

--- components/layouts/blocks/Header.js
+++ components/layouts/blocks/Header.js
@@ -1,93 +1,45 @@
 import { styled } from "../../../stitches.config.js";
 import Toolbar from "../../toolbar/Toolbar.js";
 import React, { useState } from "react";
-import Avatar from "../../avatar/Avatar.js";
+import AvatarStatusBadgePopover from "../../popover/AvatarStatusBadgePopover.js";
 
-const Container = styled("div", {
+const HeaderContainer = styled("div", {
   display: "flex",
-  flexDirection: "column-reverse",
   justifyContent: "space-between",
-  padding: "24px",
-  backgroundColor: "transparent",
   alignItems: "center",
+  padding: "$spacing-04",
+  backgroundColor: "transparent",
 
   "@bp1": {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
+    padding: "$spacing-05",
+  }
 });
 
 const Box = styled("div", {
   display: "flex",
   alignItems: "center",
+  justifyContent: "center",
+  width: "196px",
+
+  "@bp1" : {
+    width: "232px",
+  }
 });
 
-const Logo = styled("div", {
-  display: "flex",
-  size: "$8",
-  mr: "$spacing-03",
-});
+const StyledLogo = styled("img", {
+  width: "100%",
+  height: "auto",
+})
 
-const IconBox = styled("div", {
-  display: "flex",
-  size: "$7",
-  mr: "$spacing-03",
-});
-
-const Title = styled("p", {
-  display: "flex",
-  alignItems: "center",
-  gap: "$spacing-03",
-  fontFamily: "$header",
-  fontWeight: "$extra-bold",
-  lineHeight: "$compact",
-  letterSpacing: "$tracking-tighter",
-  color: "$mauve12",
-  fontSize: "$2xl",
-  mt: "$spacing-02",
-
-  "@bp2": {
-    fontSize: "$3xl",
-  },
-});
-
-const StyledSpan = styled("span", {
-  fontFamily: "$header",
-  fontWeight: "$extra-bold",
-  lineHeight: "$compact",
-  letterSpacing: "$tracking-normal",
-  fontSize: "$lg",
-  backgroundColor: "$crimsonA8",
-  pr: "8px",
-  pl: "8px",
-  pt: "6px",
-  pb: "4px",
-  borderRadius: "4px",
-  color: "$crimson2",
-  boxShadow: "$smooth",
-  mt: "-8px",
-});
-
-const ToolbarContainer = styled("div", {
-  display: "none",
-
-  "@bp3": {
-    display: "flex",
-  },
-});
 
 function Header() {
   return (
-    <Container>
+    <HeaderContainer>
       <Box>
-        <img src="/logo/logo.png" />
+        <StyledLogo src="/logo/logo.png" />
       </Box>
-      <ToolbarContainer>
-        <Toolbar />
-      </ToolbarContainer>
-      <Avatar />
-    </Container>
+      <AvatarStatusBadgePopover />
+    </HeaderContainer>
   );
 }
 
