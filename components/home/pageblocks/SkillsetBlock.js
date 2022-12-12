a1c5a963e06d250293d3814cab1d32724d1073cf
Tom Oostewechel <github.ouyis@simplelogin.com>
2022-12-12 17:11:50 +0000

    update image

--- components/home/pageblocks/SkillsetBlock.js
+++ components/home/pageblocks/SkillsetBlock.js
@@ -83,7 +83,7 @@ function SkillsetBlock() {
         <Image>
           <img
             style={{ maxWidth: "100%", height: "auto" }}
-            src="/images/skillset-placeholder.png"
+            src="/images/skillset-web.png"
           />
         </Image>
       </GridContainer>
