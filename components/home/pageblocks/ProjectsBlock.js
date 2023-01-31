8fbc73ebdbb76affcb41f556c3a00d211e0f39f6
Tom Oostewechel <github.ouyis@simplelogin.com>
2023-01-31 08:15:33 +0000

    rewrite component logic of showControls
    rewrite showControls logic and implement React.Children API
    
    rewrite showControls logic and implement react children API

--- components/home/pageblocks/ProjectsBlock.js
+++ components/home/pageblocks/ProjectsBlock.js
@@ -6,74 +6,74 @@ import CardHeader from "../../card/CardHeader.js";
 import ProjectTimelineCard from "../../card/ProjectTimelineCard.js";
 
 function ProjectsBlock() {
-  return (
-    <Section id="projects">
-      <ContentBlock
-        heading="Snapshots"
-        alignment="left"
-        description="A curated overview of my professional and personal work throughout the years as product designer"
-      />
-      <Spacer level={6} />
-      <Timeline>
-        <TimelineItem id="timeline-item">
-          <CardHeader
-            color="crimson"
-            hasYear={true}
-            alignment="left"
-            year="2022"
-          />
-          <ProjectTimelineCard
-            background="/images/placeholder-project-yellow.png"
-            english
-            title="Design and Develop a Portfolio Site in Next.JS"
-            meta="Case Study • 20 min"
-          />
-        </TimelineItem>
-        <TimelineItem>
-          <CardHeader
-            color="blue"
-            hasYear={true}
-            alignment="left"
-            year="2021"
-          />
-          <ProjectTimelineCard
-            background="/images/placeholder-project-green.png"
-            english
-            title="Building standardised eHealth modules for healthcare"
-            meta="Case Study • 20 min"
-          />
-        </TimelineItem>
-        <TimelineItem>
-          <CardHeader
-            color="plum"
-            hasYear={true}
-            alignment="right"
-            year="2022"
-          />
-          <ProjectTimelineCard
-            background="/images/placeholder-project-blue.png"
-            dutch
-            title="De verborgen complexiteit achter noodtoegang"
-            meta="Case Study • 15 min"
-          />
-        </TimelineItem>
-        <TimelineItem>
-          <CardHeader
-            color="blue"
-            hasYear={true}
-            alignment="left"
-            year="2021"
-          />
-          <ProjectTimelineCard
-            background="/images/placeholder-project-crimson.png"
-            dutch
-            title="Gestandaardiseerde e-health modules ontwikkelen"
-            meta="Case Study • 20 min"
-          />
-        </TimelineItem>
-      </Timeline>
-    </Section>
-  );
+	return (
+		<Section id="projects">
+			<ContentBlock
+				heading="Snapshots"
+				alignment="left"
+				description="A curated overview of my professional and personal work throughout the years as product designer"
+			/>
+			<Spacer level={6} />
+			<Timeline>
+				<TimelineItem id="timeline-item">
+					<CardHeader
+						color="crimson"
+						hasYear={true}
+						alignment="left"
+						year="2022"
+					/>
+					<ProjectTimelineCard
+						background="/images/placeholder-project-yellow.png"
+						english
+						title="Design and Develop a Portfolio Site in Next.JS"
+						meta="Case Study • 20 min"
+					/>
+				</TimelineItem>
+				<TimelineItem>
+					<CardHeader
+						color="blue"
+						hasYear={true}
+						alignment="left"
+						year="2021"
+					/>
+					<ProjectTimelineCard
+						background="/images/placeholder-project-green.png"
+						english
+						title="Building standardised eHealth modules for healthcare"
+						meta="Case Study • 20 min"
+					/>
+				</TimelineItem>
+				<TimelineItem>
+					<CardHeader
+						color="plum"
+						hasYear={true}
+						alignment="right"
+						year="2022"
+					/>
+					<ProjectTimelineCard
+						background="/images/placeholder-project-blue.png"
+						dutch
+						title="De verborgen complexiteit achter noodtoegang"
+						meta="Case Study • 15 min"
+					/>
+				</TimelineItem>
+				<TimelineItem>
+					<CardHeader
+						color="blue"
+						hasYear={true}
+						alignment="left"
+						year="2021"
+					/>
+					<ProjectTimelineCard
+						background="/images/placeholder-project-crimson.png"
+						dutch
+						title="Gestandaardiseerde e-health modules ontwikkelen"
+						meta="Case Study • 20 min"
+					/>
+				</TimelineItem>
+			</Timeline>
+		</Section>
+	);
 }
 
 export default ProjectsBlock;
--- components/timeline/Timeline.js
+++ components/timeline/Timeline.js
@@ -1,167 +1,131 @@
 import { styled } from "../../stitches.config.js";
-import React, { useRef, useEffect, useState } from "react";
+import React, { useRef, useEffect, useState, Children } from "react";
 import ArrowRightLineIcon from "remixicon-react/ArrowRightLineIcon";
 import ArrowLeftLineIcon from "remixicon-react/ArrowLeftLineIcon";
 import { useMediaQuery } from "react-responsive";
 
 const TimelineWrapper = styled("div", {
-	maxWidth: "1234px",
-	margin: "0 auto",
-	padding: "$spacing-05 $none",
-	overflow: "visible",
+  maxWidth: "1234px",
+  margin: "0 auto",
+  padding: "$spacing-05 $none",
+  overflow: "visible",
 });
 
 const TimelineItems = styled("ul", {
-	display: "flex",
-	padding: "$none",
-	listStyle: "none",
-	overflowX: "scroll",
-	scrollSnapType: "x",
-	scrollBehavior: "smooth",
-	"-webkit-overflow-scrolling": "touch",
-	"&::-webkit-scrollbar": {
-		display: "none",
-	},
+  display: "flex",
+  padding: "$none",
+  listStyle: "none",
+  overflowX: "scroll",
+  scrollSnapType: "x",
+  scrollBehavior: "smooth",
+  "-webkit-overflow-scrolling": "touch",
+  "&::-webkit-scrollbar": {
+    display: "none",
+  },
 });
 
 const StyledTimelineItem = styled("li", {
-	display: "flex",
-	flexDirection: "column",
-	flex: "0 0 100%",
-	height: "auto",
-	borderRadius: "16px",
-	scrollSnapAlign: "start",
-	mr: "$spacing-04",
-	pb: "$spacing-08",
+  display: "flex",
+  flexDirection: "column",
+  flex: "0 0 100%",
+  height: "auto",
+  borderRadius: "16px",
+  scrollSnapAlign: "start",
+  mr: "$spacing-04",
+  pb: "$spacing-08",
 
-	"&:last-child": {
-		mr: 0,
-	},
+  "&:last-child": {
+    mr: 0,
+  },
 
-	"@bp2": {
-		flexBasis: "calc(50% - 12px)",
-	},
-	"@bp4": {
-		flexBasis: "calc(calc(100% / 3) - 20px)",
-		mr: "$spacing-06",
-	},
-});
-
-const Card = styled("div", {
-	padding: "12px",
-	borderRadius: "16px",
-	background: "$crimson3",
+  "@bp2": {
+    flexBasis: "calc(50% - 12px)",
+  },
+  "@bp4": {
+    flexBasis: "calc(calc(100% / 3) - 20px)",
+    mr: "$spacing-06",
+  },
 });
 
 const ControlsContainer = styled("div", {
-	display: "flex",
-	flexDirection: "row",
-	alignItems: "center",
-	width: "100%",
-	gap: "$spacing-04",
-	background: "transparent",
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  width: "100%",
+  gap: "$spacing-04",
+  background: "transparent",
 });
 
 const Controls = styled("div", {
-	display: "flex",
-	flexDirection: "row",
-	flexShrink: 0,
+  display: "flex",
+  flexDirection: "row",
+  flexShrink: 0,
 });
 
 const Line = styled("div", {
-	height: "1px",
-	background: "$gray6",
-	width: "100%",
+  height: "1px",
+  background: "$gray6",
+  width: "100%",
 });
 
 const Button = styled("button", {
-	padding: "$spacing-04",
-	backgroundColor: "white",
-	border: "1px solid $gray6",
-	borderRadius: "8px",
-	mr: "8px",
-	color: "$violet11",
+  padding: "$spacing-04",
+  backgroundColor: "white",
+  border: "1px solid $gray6",
+  borderRadius: "8px",
+  mr: "8px",
+  color: "$violet11",
 
-	"&:last-child": {
-		mr: 0,
-	},
+  "&:last-child": {
+    mr: 0,
+  },
 
-	"&:hover": {
-		backgroundColor: "$violet3",
-	},
-	"&:active": {
-		backgroundColor: "$violet4",
-	},
+  "&:hover": {
+    backgroundColor: "$violet3",
+  },
+  "&:active": {
+    backgroundColor: "$violet4",
+  },
 });
 
+//Helper Function to get TimelineCard Width
 function getCardWidth() {
-	return document.getElementById("timeline-item").clientWidth;
+  return document.getElementById("timeline-item").clientWidth;
 };
 
 export const TimelineItem = StyledTimelineItem;
 export const Timeline = ({ children }) => {
-	const [count, setCount] = useState(0);
-	const ref = useRef(null);
-	const scroll = (scrollOffset) => {
-		ref.current.scrollLeft += scrollOffset;
-	};
-	const isMobile = useMediaQuery({ maxWidth: 479 });
-	const bp1 = useMediaQuery({ minWidth: 480, maxWidth: 640 });
-	const bp2 = useMediaQuery({ minWidth: 640, maxWidth: 768 });
-	const bp3 = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
-	const bp4 = useMediaQuery({ minWidth: 1024 });
+	//Scroll Function
+  const ref = useRef(null);
+  const scroll = (scrollOffset) => { ref.current.scrollLeft += scrollOffset; };
 
-	useEffect(() => {
-		const elementCount = ref.current.childNodes.length
-		setCount(elementCount);
-	})
+	//On BP4 - BP3 => 3 Items in Viewport || On BP2, 2 Items in Viewport || On BP1, 1 Item in Viewport
+  const bp2 = useMediaQuery({ minWidth: 640 }) 
+  const bp4 = useMediaQuery({ minWidth: 1024 }); 
+
+	//Conditionally Render Controls based on Viewport and timelineCount
+	const [count, setCount] = useState(0);
+	const timelineCount = Children.count(children);
+	const showControls = bp4 ? count > 3 : bp2 ? count > 2 : count > 1;
 	
-	return (
-		<TimelineWrapper>
-			<TimelineItems ref={ref}>{children}</TimelineItems>
-			<ControlsContainer>
-				<Line />
-				{ count > 1 && isMobile ? (<Controls>
-					<Button onClick={() => scroll(-getCardWidth())}>
-						<ArrowLeftLineIcon />
-					</Button>
-					<Button onClick={() => scroll(getCardWidth())}>
-						<ArrowRightLineIcon />
-					</Button>
-				</Controls>) : null}
-				{ count > 1 && bp1 ? (<Controls>
-					<Button onClick={() => scroll(-getCardWidth())}>
-						<ArrowLeftLineIcon />
-					</Button>
-					<Button onClick={() => scroll(getCardWidth())}>
-						<ArrowRightLineIcon />
-					</Button>
-				</Controls>) : null}
-				{ count > 2 && bp2 ? (<Controls>
-					<Button onClick={() => scroll(-getCardWidth())}>
-						<ArrowLeftLineIcon />
-					</Button>
-					<Button onClick={() => scroll(getCardWidth())}>
-						<ArrowRightLineIcon />
-					</Button>
-				</Controls>) : null}
-				{ count > 2 && bp3 ? (<Controls>
-					<Button onClick={() => scroll(-getCardWidth())}>
-						<ArrowLeftLineIcon />
-					</Button>
-					<Button onClick={() => scroll(getCardWidth())}>
-						<ArrowRightLineIcon />
-					</Button>
-				</Controls>) : null}
-				{ count > 3 && bp4 ? (<Controls>
-					<Button onClick={() => scroll(-getCardWidth())}>
-						<ArrowLeftLineIcon />
-					</Button>
-					<Button onClick={() => scroll(getCardWidth())}>
-						<ArrowRightLineIcon />
-					</Button>
-				</Controls>) : null}
-			</ControlsContainer>
-		</TimelineWrapper>
-	);
+  useEffect(() => {
+		setCount(timelineCount);
+  }, [setCount])
+
+  return (
+    <TimelineWrapper>
+      <TimelineItems ref={ref}>{children}</TimelineItems>
+      <ControlsContainer>
+        <Line />
+        {showControls ? (<Controls>
+          <Button onClick={() => scroll(-getCardWidth())}>
+            <ArrowLeftLineIcon />
+          </Button>
+          <Button onClick={() => scroll(getCardWidth())}>
+            <ArrowRightLineIcon />
+          </Button>
+        </Controls>) : null}
+      </ControlsContainer>
+    </TimelineWrapper>
+  );
 };
