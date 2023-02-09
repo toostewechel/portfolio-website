06dca7f064b2135290a2fe1bfeb01b5460495031
Tom Oostewechel <github.ouyis@simplelogin.com>
2023-02-09 08:16:51 +0000

    rename ToC components

---
+++ components/toc/TableOfContentSection.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from 'react';
+
+import Heading from "../typography/Heading";
+
+interface Props {
+	topic: string;
+	children: ReactNode
+	chapter: string;
+}
+
+const TableOfContentSection: FC<Props> = ({ topic, children, chapter }) => {
+	return (
+		<section id={topic.toLowerCase()} className={`section-heading-${chapter}`}>
+			<Heading as="h2" title={topic} level="3" />
+			<div>{children}</div>
+		</section>
+	);
+}
+
+export default TableOfContentSection;
