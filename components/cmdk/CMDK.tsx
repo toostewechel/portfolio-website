2dd17444e042d166df0f45c38f58b61c987ff143
toostewechel <github.ouyis@simplelogin.com>
2023-04-10 11:29:32 +0200

    add email and phone to cmdk menu

--- components/cmdk/CMDK.tsx
+++ components/cmdk/CMDK.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { styled } from "../../stitches.config.js";
 import { Command } from "cmdk";
 import { useRouter } from "next/router";
+import { Twitter, Github, Linkedin, Smartphone, Mail } from "lucide-react";
 
 const StyledCommand = styled(Command, {
   boxSizing: "border-box",
@@ -328,13 +329,14 @@ function Dashboard({
       </CommandGroup>
       <CommandGroup heading="Socials">
         <Item onSelect={() => router.push("https://twitter.com/boonikad93")}>
-          <TwitterIcon />
+          <Twitter size={20} />
           View Twitter Profile ...
         </Item>
         <Item onSelect={() => router.push("https://github.com/toostewechel")}>
-          <GithubIcon />
+          <Github size={20} />
           View GitHub Profile ...
         </Item>
+
         <Item
           onSelect={() =>
             router.push(
@@ -342,8 +344,16 @@ function Dashboard({
             )
           }
         >
-          <LinkedInIcon />
+          <Linkedin size={20} />
           View LinkedIn Profile ...
+        </Item>
+        <Item onSelect={() => router.push("mailto:tomas93@skiff.com")}>
+          <Mail size={20} />
+          E-mail
+        </Item>
+        <Item onSelect={() => router.push("tel:0612647976")}>
+          <Smartphone size={20} />
+          Phone Number
         </Item>
       </CommandGroup>
     </>
@@ -447,81 +457,6 @@ function BlogIcon() {
     >
       <path d="M12 20h9"></path>
       <path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path>
-    </svg>
-  );
-}
-
-function CopyIcon() {
-  return (
-    <svg
-      fill="none"
-      height="24"
-      shapeRendering="geometricPrecision"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="1.5"
-      viewBox="0 0 24 24"
-      width="24"
-    >
-      <path d="M8 17.929H6c-1.105 0-2-.912-2-2.036V5.036C4 3.91 4.895 3 6 3h8c1.105 0 2 .911 2 2.036v1.866m-6 .17h8c1.105 0 2 .91 2 2.035v10.857C20 21.09 19.105 22 18 22h-8c-1.105 0-2-.911-2-2.036V9.107c0-1.124.895-2.036 2-2.036z"></path>
-    </svg>
-  );
-}
-
-function TwitterIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
-    </svg>
-  );
-}
-
-function GithubIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-      <path d="M9 18c-4.51 2-5-2-7-2"></path>
-    </svg>
-  );
-}
-
-function LinkedInIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      stroke-width="1.5"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-    >
-      <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-      <rect x="2" y="9" width="4" height="12"></rect>
-      <circle cx="4" cy="4" r="2"></circle>
     </svg>
   );
 }
