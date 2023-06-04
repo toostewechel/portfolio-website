98bce15fd1a34b06e273825d220f599a3e68a9f8
Tom Oostewechel <github.ouyis@simplelogin.com>
2023-06-04 11:28:43 +0200

    set caching headers

--- next.config.js
+++ next.config.js
@@ -6,8 +6,22 @@ const withMdx = nextMdx({
   options: { providerImportSource: "@mdx-js/react" /* otherOptions… */ },
 });
 
-export default withMdx({
+module.exports = withMdx({
   // Support MDX files as pages:
   pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
   output: "standalone",
+  async headers() {
+    return [
+      {
+        // Set caching headers for font files
+        source: "/fonts/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 });
